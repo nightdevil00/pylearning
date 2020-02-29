9a637eb71226b2adcbf9f57ecc83b066de001771
nightdevil00 <49100220+nightdevil00@users.noreply.github.com>
2020-02-29 16:21:12 +0200

    Add files via upload
    a small txt viewer

---
+++ fileviewer.py
@@ -0,0 +1,56 @@
+from tkinter import Frame, Tk, BOTH, Text, Menu, END
+from tkinter import filedialog
+
+class Example(Frame):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.master.title("File dialog")
+        self.pack(fill=BOTH, expand=1)
+
+        menubar = Menu(self.master)
+        self.master.config(menu=menubar)
+
+        fileMenu = Menu(menubar)
+        fileMenu.add_command(label="Open", command=self.onOpen)
+        menubar.add_cascade(label="File", menu=fileMenu)
+
+        self.txt = Text(self)
+        self.txt.pack(fill=BOTH, expand=1)
+
+
+    def onOpen(self):
+
+        ftypes = [('TXT files', '*.txt'), ('All files', '*')]
+        dlg = filedialog.Open(self, filetypes = ftypes)
+        fl = dlg.show()
+
+        if fl != '':
+            text = self.readFile(fl)
+            self.txt.insert(END, text)
+
+
+    def readFile(self, filename):
+
+        with open(filename, "r") as f:
+            text = f.read()
+
+        return text
+
+
+def main():
+
+    root = Tk()
+    ex = Example()
+    root.geometry("300x250+300+300")
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
